5f4a496c3ec3e69f65629d4d55a24d191916fbe5
Tara <tarachandsleeping@gmail.com>
2021-08-12 09:27:18 +0530

    added code

--- vehicle.go
+++ vehicle.go
@@ -1,18 +1,27 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
 type vehicle interface {
 }
 
 type car struct {
 	model       string
 	make        string
-	typeVehivle string
+	typeVehicle string
 }
 
 type truck struct {
 	model       string
 	make        string
-	typeVehivle string
+	typeVehicle string
 }
 
 type bike struct {
@@ -20,31 +29,27 @@ type bike struct {
 	make  string
 }
 
-//values array
+// Values array for the feedback.json file
 type Values struct {
 	Models []Model `json:"values"`
 }
 
-// model array
+// Model array for the feedback.json file
 type Model struct {
 	Name     string   `json:"model"`
 	Feedback []string `json:"feedback"`
 }
+
 type feedbackResult struct {
 	feedbackTotal    int
 	feedbackPositive int
 	feedbackNegative int
 	feedbackNeutral  int
 }
-type comment struct {
-}
 
 var vehicleResult map[string]feedbackResult
+
 var inventory []vehicle
-
-// Values array for the feedback.json file
-
-// Model array for the feedback.json file
 
 type rating float32
 
@@ -78,12 +83,26 @@ func init() {
 func main() {
 
 	// Generate ratings for the different vehicles
-
+	generateRating()
 	// Print ratings for the different vehicles
+
+	for _, veh := range inventory {
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+	}
+
 }
 
-/*
 func readJSONFile() Values {
+
 	jsonFile, err := os.Open("feedback.json")
 
 	if err != nil {
@@ -98,4 +117,72 @@ func readJSONFile() Values {
 
 	return content
 }
-*/
+
+func generateRating() {
+
+	f := readJSONFile()
+
+	for _, v := range f.Models {
+		var vehResult feedbackResult
+		var vehRating rating
+		for _, msg := range v.Feedback {
+
+			if text := strings.Split(msg, " "); len(text) >= 5 {
+				vehRating = 5.0
+				vehResult.feedbackTotal++
+				for _, word := range text {
+					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
+					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+						vehRating += extraPositive
+					case "help", "helpful", "thanks", "thank you", "happy":
+						vehRating += positive
+					case "not helpful", "sad", "angry", "improve", "annoy":
+						vehRating += negative
+					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+						vehRating += extraNegative
+					}
+
+				}
+				switch {
+				case vehRating > 8.0:
+					vehResult.feedbackPositive++
+				case vehRating >= 4.0 && vehRating <= 8.0:
+					vehResult.feedbackNeutral++
+				case vehRating < 4.0:
+					vehResult.feedbackNegative++
+				}
+			}
+		}
+		vehicleResult[v.Name] = vehResult
+	}
+
+}
+
+func showRating(model string) {
+	ratingFound := false
+	for m, r := range vehicleResult {
+		if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
+		}
+	}
+	if !ratingFound {
+		fmt.Printf("No rating found for this vehicle")
+	}
+
+}
+
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car" , c.make, c.model)
+	showRating(c.model)
+}
+
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
+	showRating(b.model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
+	showRating(t.model)
+}
